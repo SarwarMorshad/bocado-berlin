87b68e68050d6ab9b7915ad38cccba2de78362f1
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-11-14 13:21:52 +0100

    Setting Up Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold">Vite + React</h1>
+      <button class="btn btn-error">Error</button>
+    </>
+  );
+}
+
+export default App;
